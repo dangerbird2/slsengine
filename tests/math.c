7e3f13372a452add23d7dbadf2256e53e0032bc5
Steve Shea <stevenlsjr@gmail.com>
2018-07-21 19:23:42 -0400

    added minunit test runner

--- tests/math.c
+++ tests/math.c
@@ -2,12 +2,16 @@
 #include <common.h>
 #include <slsmath.h>
 
+#define MINUNIT_EPSILON 0.01
+
+#include <minunit.h>
+
 #ifdef NDEBUG
 #undef NDEBUG
 #endif
 
 
-void dot_tests()
+MU_TEST(dot_product_test)
 {
   slsVec2 v0, v1;
   slsVec3 v3, v4;
@@ -16,9 +20,9 @@ void dot_tests()
   // test dot product
   v0 = sls_vec2_make(-62.47096805f, -5.87299575f);
   v1 = sls_vec2_make(-217.38856733f, 14.50296503f);
-  fexpected = 13495.2983928676f;
+  fexpected = 13495.298f;
   fresult = sls_vec2_dot(&v0, &v1);
-  assert(sls_nearf(fexpected, fresult));
+  mu_check(sls_nearf(fexpected, fresult));
 
 
   v3 = sls_vec3_make(24.814496f, -7.05247768f, -223.76167351f);
@@ -26,11 +30,18 @@ void dot_tests()
   fexpected = -21230.522f;
   fresult = sls_vec3_dot(&v3, &v4);
 
-  assert(sls_nearf(fexpected, fresult));
+  mu_check(sls_nearf(fexpected, fresult));
 
+}
+
+MU_TEST_SUITE(math_suite)
+{
+  MU_RUN_TEST(dot_product_test);
 }
 
 void run_math_tests()
 {
-  dot_tests();
+  MU_RUN_SUITE(math_suite);
+  MU_REPORT();
+
 }
